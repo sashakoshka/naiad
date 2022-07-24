a1c72ce2419b4b658439b6c931957061c042b44c
Sasha Koshka <supergriffin64@gmail.com>
2022-07-24 04:19:15 -0400

    Added stub for shape group

---
+++ shapegroup.go
@@ -0,0 +1,46 @@
+package naiad
+
+import "github.com/faiface/pixel/imdraw"
+
+// TODO: put this at the root of the window instead of a list of shapes.
+type ShapeGroup struct {
+	shapeBase
+	shapes []Shape
+}
+
+func NewShapeGroup (
+	x, y float64,
+) (
+	group *ShapeGroup,
+) {
+	group = &ShapeGroup { }
+	group.SetPosition(V(x, y))
+	return
+}
+
+func (group *ShapeGroup) Push (shape Shape) {
+	group.shapes = append(group.shapes, shape)
+	group.calculateBounds()
+	group.SetDirty()
+}
+
+func (group *ShapeGroup) Pop () (shape Shape) {
+	shape = group.shapes[len(group.shapes) - 1]
+	group.shapes = group.shapes[:len(group.shapes) - 1]
+	group.calculateBounds()
+	group.SetDirty()
+	return
+}
+
+func (polygon *ShapeGroup) Kind () (kind ShapeKind) {
+	return ShapeKindPath
+}
+
+func (shape *ShapeGroup) draw (artist *imdraw.IMDraw) {
+	// TODO: range over shapes, and if any of them are dirty, redraw all of
+	// them to internal buffer.
+}
+
+func (group *ShapeGroup) calculateBounds () {
+	// TODO: range over shapes and yeah
+}
